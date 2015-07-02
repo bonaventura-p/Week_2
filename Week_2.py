35e597d6d0bad9be47a6ae1044ddcefa5b112518
bonaventura-p <bonaventurapacileo@gmail.com>
2015-07-02 13:12:41 +0200

    bin search

--- Week_2.py
+++ Week_2.py
@@ -1,4 +1,4 @@
-
+'''
 #turtle basic
 from turtle import *
 
@@ -51,7 +51,7 @@ class Child(object):
         self.sounds = sounds
 	def cry_baby(self):
         for sound in self.sounds:
-            print "{}m".format(sound) 
+        	print "{}m".format(sound) 
 
 class Boy(Child): # The Child class is the parent of the Boy class
     def __init__(self):
@@ -66,4 +66,89 @@ class Girl(Child):
     def drink_milk(self):
         print "I'm going to drink Milk"
         print "Twoning, sproing, splang splart... "
+
+#linear search
+x=[1,2,3,5,4,10]
+target=5
+def linear_search(x,target):
+	position=-1
+	count=0
+	while count <len(x):
+		if x[count]==target:
+			position=count
+		count +=1
+	if position ==-1:
+		print "Target {} hasn't been found".format(target)
+	else:
+		print "Target {} found at position{}",format(target,position)
+   #the output is 3 and indeed 5 is at position 3
+#recall we start counting from 0
+'''
+#binary search
+
+def median(x):
+	x=sorted(x)
+	if len(x)%2==0:
+		return float(sum(x[(len(x)/2)-1:(len(x)/2)+1]))/2.0
+	else:
+		return x[((len(x)+1)/2)-1]
+
+target=5
+x=[0,1,2,3,4,5,7,8]
+def binary_search(x,target):
+	midpoint=int(median(x))
+	if x==[]:
+		minx=0
+	else:
+		minx=min(x)
+	if x==[]:
+		maxx=0
+	else:
+		maxx=max(x)	
+	if target >maxx or target <minx:
+		return "Not in the list"
+	if target<midpoint:
+		x=x[minx:midpoint-1]
+		binary_search(x,target)
+	elif target > midpoint:
+		x=x[midpoint+1:maxx]
+		binary_search(x,target)
+	elif target==midpoint:
+		 position=midpoint
+		 print position
+
+def binary_search1(x,target):
+	if x==[]:
+		minx=0
+	else:
+		minx=min(x)
+	if x==[]:
+		maxx=0
+	else:
+		maxx=max(x)	
+	while True:
+    	if maxx < minx:
+        	return -1
+        midpoint = (minx + maxx) // 2
+        if x[midpoint] < target:
+            minx = midpoint + 1
+        elif x[midpoint] > target:
+            maxx = midpoint - 1
+        else:
+            return midpoint
+			
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
